52e7f4d298b1e7637a14c9c09d33685ccc88eb83
Steve Wattam <stephenwattam@gmail.com>
2012-09-04 20:42:41 +0100

    Added CSVCat, a concatenation tool.

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-./csvmerge.rb test/sample_orig.csv test/sample_addit.csv out.csv InnerMerge MemoryCache url -- url
+./csvmerge test/sample_orig.csv test/sample_addit.csv out.csv InnerMerge MemoryCache url -- url
 
 
-./csvsplit.rb out.csv one.csv url count -- two.csv count2
+./csvsplit out.csv one.csv url count -- two.csv count2
+
+
+./csvcat test/sample_orig.csv test/sample_huge.csv test/sample_addit.csv out.csv
